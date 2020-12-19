8c658da719d3cc94fbf4dab0316ce05d14c15977
CepheidVariable <robertwoodard.usa@gmail.com>
2020-12-19 01:25:27 -0600

    preview done

--- script.js
+++ script.js
@@ -10,14 +10,72 @@ $(document).ready(function() {
 
     $("#world_preview").addClass("flexing");
     $("#world_preview img").attr("alt", "World Gallery Image");
+    $(".slider p").mouseover(function(){
+      $(".slider p").alert();
+      $(".slider p").css("transform","translate(- 240 px)");
+    });
+    // $(".btn").click(function(){
+    //   $(".btn").
+    // });
+
+
+    $(".slideShow1").click(function(){
+      $(".dropdown1").slideToggle();
+      $(".dropdown2").hide();
+      $(".dropdown3").hide();
+      $(".dropdown4").hide();
+    });
+    $(".slideShow2").click(function(){
+      $(".dropdown2").slideToggle();
+      $(".dropdown1").hide();
+      $(".dropdown3").hide();
+      $(".dropdown4").hide();
+    });
+    $(".slideShow3").click(function(){
+      $(".dropdown3").slideToggle();
+      $(".dropdown1").hide();
+      $(".dropdown2").hide();
+      $(".dropdown4").hide();
+    });
+    $(".slideShow4").click(function(){
+      $(".dropdown4").slideToggle();
+      $(".dropdown1").hide();
+      $(".dropdown2").hide();
+      $(".dropdown3").hide();
+
+    });
+
+
+
     // $("#world_preview .prevBox3").hide();
     // $(".slideShow1 img").fadeOut(500);
 });
 
 // $('#slideShow1 img:gt(0)').hide();
 
-var speed = 6000;
-run = setInterval("test()", speed);
-function test(){
-  $('.slideShow1 img:first-child').fadeOut(500).next().show().end().appendTo('.slideShow1');
+var speed1 = 8000;
+var speed2 = 4000;
+var speed3 = 6000;
+var speed4 = 12000;
+
+run = setInterval("slideShow1()", speed1);
+run = setInterval("slideShow2()", speed2);
+run = setInterval("slideShow3()", speed3);
+run = setInterval("slideShow4()", speed4);
+
+function slideShow1(){
+  $('.slideShow1 img:first-child').fadeOut(800).next().show().appendTo('.slideShow1');
 }
+
+function slideShow2(){
+  $('.slideShow2 img:first-child').fadeOut(800).next().show().end().appendTo('.slideShow2');
+}
+
+function slideShow3(){
+  $('.slideShow3 img:first-child').fadeOut(800).next().show().end().appendTo('.slideShow3');
+}
+
+function slideShow4(){
+  $('.slideShow4 img:first-child').fadeOut(800).next().show().end().appendTo('.slideShow4');
+}
+
